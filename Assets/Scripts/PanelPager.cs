32d8fc30ab6a9bfaecdf9df9acc62cec0da6115b
Daphne Liu <daphnel15113455@gmail.com>
2018-02-27 23:49:37 -0800

    Improved PanelPager code & layout, got video to work

--- Assets/Scripts/PanelPager.cs
+++ Assets/Scripts/PanelPager.cs
@@ -1,13 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq; // used for Sum of array
+using System.Linq;
 
+/// <summary>
+/// Lets the user page through panels set as children of this object.
+/// </summary>
 public class PanelPager : MonoBehaviour
 {
+    /// <summary>Name of the tag used to identify panels</summary>
     public string panelTag = "Panel";
+    /// <summary>List of panels</summary>
     public List<GameObject> panels = new List<GameObject>();
 
+    /// <summary>Button to move to the previous panel</summary>
+    public Button previousButton = null;
+    /// <summary>Button to move to the next panel</summary>
+    public Button nextButton = null;
+
+    /// <summary>Index of the currently active panel</summary>
     int currentIndex = 0;
 
     void Start()
@@ -15,17 +27,25 @@ public class PanelPager : MonoBehaviour
         // Auto-fill panels list
         if (panels.Count == 0)
         {
-            var i = 0;
-            foreach (Transform child in transform)
+            var panelsQuery = from child in transform.GetComponentsInChildren<Transform>(true)
+                              let gameObject = child.gameObject
+                              where gameObject.tag == panelTag
+                              select gameObject;
+
+            panels = panelsQuery.Select((child, i) =>
             {
-                if (child.gameObject.tag == panelTag)
-                {
-                    panels.Add(child.gameObject);
-                    child.gameObject.SetActive(i == currentIndex);
-                }
-                i++;
-            }
+                child.SetActive(i == currentIndex);
+                return child;
+            }).ToList();
         }
+
+        SetButtonsInteractable();
+    }
+
+    private void SetButtonsInteractable()
+    {
+        if (previousButton) previousButton.interactable = currentIndex > 0;
+        if (nextButton) nextButton.interactable = currentIndex < panels.Count - 1;
     }
 
     private void ChangeActivePanel(int index)
@@ -35,6 +55,8 @@ public class PanelPager : MonoBehaviour
         panels[currentIndex].SetActive(false);
         panels[index].SetActive(true);
         currentIndex = index;
+
+        SetButtonsInteractable();
     }
 
     public void PreviousPanel()
