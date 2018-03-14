8424ec0df909729b2a6a58fdd0e101a0b305f7e5
Daphne Liu <daphnel15113455@gmail.com>
2018-03-13 21:52:36 -0700

    added PlayVideo.cs

--- Assets/Scripts/PlayVideo.cs
+++ Assets/Scripts/PlayVideo.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayVideo : MonoBehaviour {
+public class PlayVideo : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
