3e7d107d0b5efd98f4388ef6f7bba009772c2e98
Daphne Liu <daphnel15113455@gmail.com>
2018-03-14 22:18:22 -0700

    Changed brackets

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,40 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
-	 public float speed;
-     public GameObject target;
- 
-     void Update () {
- 
-         if (Input.GetMouseButton(0)) {
-             transform.RotateAround(
-                 target.transform.position,
-                 target.transform.up,
-                 Input.GetAxis("Mouse X") * speed);
-             transform.RotateAround(
-                 target.transform.position,
-                 target.transform.right,
-                 Input.GetAxis("Mouse Y") * speed);
-         }
-         transform.LookAt(target.transform, target.transform.up);
-     }
+    public float speed;
+    public GameObject target;
 
-	 if (Input.GetAxis ("Mouse ScrollWheel") > 0)
-	 {
-		 GetComponent<Transform>().position = new Vector3(
-			 transform.position.x, 
-		 	 transform.position.y - 0.4f, 
-			 transform.position.z + 0.4f);
-		//  transform.Rotate(-4, 0, 0);
-	 }
-	 if (Input.GetAxis ("Mouse ScrollWheel") < 0)
-	 {
-		 GetComponent<Transform>().position = new Vector3(
-			 transform.position.x, 
-		 	 transform.position.y + 0.4f, 
-			 transform.position.z - 0.4f);
-		//  transform.Rotate(-2, 0, 0);
-	 }
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            transform.RotateAround(
+                target.transform.position,
+                target.transform.up,
+                Input.GetAxis("Mouse X") * speed);
+            transform.RotateAround(
+                target.transform.position,
+                target.transform.right,
+                Input.GetAxis("Mouse Y") * speed);
+        }
+        transform.LookAt(target.transform, target.transform.up);
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            GetComponent<Transform>().position = new Vector3(
+                transform.position.x,
+                transform.position.y - 0.4f,
+                transform.position.z + 0.4f);
+            //  transform.Rotate(-4, 0, 0);
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            GetComponent<Transform>().position = new Vector3(
+                transform.position.x,
+                transform.position.y + 0.4f,
+                transform.position.z - 0.4f);
+            //  transform.Rotate(-2, 0, 0);
+        }
+    }
 }
