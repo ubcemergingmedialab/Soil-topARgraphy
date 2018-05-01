ac82a4fd7d1d7e2b7c062f4012952339837c4859
Tiger Oakes <tigeroakes@gmail.com>
2018-05-01 13:54:05 -0700

    Added pinch to scale

---
+++ Assets/Scripts/PinchToScale.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>Scales a transform in response to the user pinching the screen</summary>
+public class PinchToScale : MonoBehaviour
+{
+    public float scaleSpeed = 0.5f;
+    public Vector3 minScale = Vector3.one * 0.1f;
+    public Vector3 maxScale = Vector3.one;
+
+    /// <summary>Find the difference in the distances between each frame.</summary>
+    static float GetTouchDeltaMagnitudeDiff()
+    {
+        // Store both touches.
+        Touch touchZero = Input.GetTouch(0);
+        Touch touchOne = Input.GetTouch(1);
+
+        // Find the position in the previous frame of each touch.
+        Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+        Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+        // Find the magnitude of the vector (the distance) between the touches in each frame.
+        float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+        float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+        // Find the difference in the distances between each frame.
+        float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+        return deltaMagnitudeDiff;
+    }
+
+    Vector3 Scale(Vector3 input, float delta)
+    {
+        Vector3 newScale = input * delta * scaleSpeed;
+        newScale = Vector3.Min(newScale, maxScale);
+        newScale = Vector3.Max(newScale, minScale);
+        return newScale;
+    }
+
+    void Update()
+    {
+        // If there are two touches on the device...
+        if (Input.touchCount == 2)
+        {
+            transform.localScale = Scale(transform.localScale, GetTouchDeltaMagnitudeDiff());
+        }
+    }
+}
--- Assets/Scripts/quizButton.cs
+++ Assets/Scripts/quizButton.cs
@@ -1,71 +1,77 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 
-public class QuizButton : MonoBehaviour {
-	/// <summary>Default title, used when quiz is reset.</summary>
-	public string title = "Hey, want to take a quiz?";
+public class QuizButton : MonoBehaviour
+{
+    /// <summary>Default title, used when quiz is reset.</summary>
+    public string title = "Hey, want to take a quiz?";
 
-	/// <summary>Reference to response textbox</summary>
-	public Text Textbox;
+    /// <summary>Reference to response textbox</summary>
+    public Text Textbox;
 
-	/// <summary>
-	/// List of possible responses when an answer is clicked.
-	/// The index of each string corresponds to the index of the button.
-	/// </summary>
-	public List<string> Responses = new List<string>() {
-		"Yeees",
-		"Aww why not?",
-		"please?",
-		"CORRECT! A+ for you"
-	};
+    /// <summary>
+    /// List of possible responses when an answer is clicked.
+    /// The index of each string corresponds to the index of the button.
+    /// </summary>
+    public List<string> Responses = new List<string>() {
+        "Yeees",
+        "Aww why not?",
+        "please?",
+        "CORRECT! A+ for you"
+    };
 
-	/// <summary>
-	/// List of buttons that can be clicked.
-	/// When an answer is selected, all other buttons will be hidden.
-	/// </summary>
-	public List<Button> AnswerButtons = new List<Button>();
-	public int ChoiceMade = -1;
+    /// <summary>
+    /// List of buttons that can be clicked.
+    /// When an answer is selected, all other buttons will be hidden.
+    /// </summary>
+    public List<Button> AnswerButtons = new List<Button>();
+    public int ChoiceMade = -1;
 
-	/// <summary>
-  /// Click listener assigned to the answer buttons. Should be called
-	/// with the index of the button.
-  /// </summary>
-	public void ChooseOption(int num) {
-		Textbox.text = num < Responses.Count ? Responses[num] : "Correct";
-		ChoiceMade = num;
+    /// <summary>
+    /// Click listener assigned to the answer buttons. Should be called
+    /// with the index of the button.
+    /// </summary>
+    public void ChooseOption(int num)
+    {
+        Textbox.text = num < Responses.Count ? Responses[num] : "Correct";
+        ChoiceMade = num;
 
-		// hide other options
-		int i = 0;
-		foreach (var button in AnswerButtons) {
-			if (i != num)	button.gameObject.SetActive(false);
-			i++;
-		}
-	}
+        // hide other options
+        int i = 0;
+        foreach (var button in AnswerButtons)
+        {
+            if (i != num) button.gameObject.SetActive(false);
+            i++;
+        }
+    }
 
-	/// <summary>
-  /// Call this function to reset the quiz panel.
-  /// </summary>
-	public void ResetQuiz() {
-		Textbox.text = title.ToUpper();
-		ChoiceMade = -1;
-		foreach (var button in AnswerButtons) {
-			button.gameObject.SetActive(true);
-		}
-	}
+    /// <summary>
+    /// Call this function to reset the quiz panel.
+    /// </summary>
+    public void ResetQuiz()
+    {
+        Textbox.text = title.ToUpper();
+        ChoiceMade = -1;
+        foreach (var button in AnswerButtons)
+        {
+            button.gameObject.SetActive(true);
+        }
+    }
 
-	/// <summary>
-	/// Setup buttons. Will reset their On Click listeners.
-	/// </summary>
-	[ContextMenu("Setup Answer Buttons")]
-	public void SetupButtons() {
-		int i = 0;
-		foreach (var button in AnswerButtons) {
-			button.onClick.RemoveAllListeners();
-			button.onClick.AddListener(() => ChooseOption(i));
-			i++;
-		}
-	}
+    /// <summary>
+    /// Setup buttons. Will reset their On Click listeners.
+    /// </summary>
+    [ContextMenu("Setup Answer Buttons")]
+    public void SetupButtons()
+    {
+        int i = 0;
+        foreach (var button in AnswerButtons)
+        {
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(() => ChooseOption(i));
+            i++;
+        }
+    }
 }
