9b9148aa5c924592ebfda76d2a2bb7d9bc84a361
Tiger Oakes <tigeroakes@gmail.com>
2018-05-03 02:00:20 -0700

    Added minified terrain mesh

--- Assets/Scripts/TerrainToggle.cs
+++ Assets/Scripts/TerrainToggle.cs
@@ -1,14 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>Handler for the Satellite/Heightmap toggle</summary>
 public class TerrainToggle : MonoBehaviour
 {
-    public GameObject Heightmap;
-    public GameObject Map;
+    /// <summary>Label indicating currently displayed terrain</summary>
+    public Text label;
+
+    public GameObject heightmap;
+    public string heightmapText = "HEIGHTMAP";
+
+    public GameObject satellite;
+    public string satelliteText = "SATELLITE";
 
     public void ToggleChanged(Toggle change)
     {
-        Heightmap.SetActive(!change.isOn);
-        Map.SetActive(change.isOn);
+        heightmap.SetActive(!change.isOn);
+        satellite.SetActive(change.isOn);
+
+        label.text = change.isOn ? satelliteText : heightmapText;
     }
 }
