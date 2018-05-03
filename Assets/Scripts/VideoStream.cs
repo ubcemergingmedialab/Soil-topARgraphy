b410eabc4e9b04adc4e03dc21a6a7a919906db49
Tiger Oakes <tigeroakes@gmail.com>
2018-05-03 03:28:44 -0700

    Moved videos to be streaming assets

---
+++ Assets/Scripts/VideoStream.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using UnityEngine;
+using UnityEngine.Video;
+
+/// <summary>
+/// Sets the URL of a Video Player to point to a streaming asset, whose
+/// location can vary platform to platform.
+/// </summary>
+[RequireComponent(typeof(VideoPlayer))]
+public class VideoStream : MonoBehaviour
+{
+    public string path;
+
+    void Start()
+    {
+        var player = GetComponent<VideoPlayer>();
+        player.url = Path.Combine(Application.streamingAssetsPath, path);
+    }
+}
