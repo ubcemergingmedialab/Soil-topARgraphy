d7e27cc684703761538e9e199d283b7f93ceca14
Tiger Oakes <tigeroakes@gmail.com>
2018-10-19 23:16:01 -0700

    Add UI scene and cards

---
+++ Assets/UI/PanelPager.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Lets the user page through panels set as children of this object.
+/// </summary>
+public class PanelPager : MonoBehaviour
+{
+    /// <summary>Name of the tag used to identify panels</summary>
+    public string panelTag = "Panel";
+    /// <summary>List of panels</summary>
+    public List<GameObject> panels = new List<GameObject>();
+
+    /// <summary>Button to move to the previous panel</summary>
+    public Button previousButton = null;
+    /// <summary>Button to move to the next panel</summary>
+    public Button nextButton = null;
+
+    /// <summary>Index of the currently active panel</summary>
+    int currentIndex = 0;
+
+    void Start()
+    {
+        // Auto-fill panels list
+        if (panels.Count == 0)
+        {
+            var panelsQuery = from child in transform.GetComponentsInChildren<Transform>(true)
+                              let gameObject = child.gameObject
+                              where gameObject.tag == panelTag
+                              select gameObject;
+
+            panels = panelsQuery.Select((child, i) =>
+            {
+                child.SetActive(i == currentIndex);
+                return child;
+            }).ToList();
+        }
+
+        SetButtonsInteractable();
+    }
+
+    private void SetButtonsInteractable()
+    {
+        if (previousButton) previousButton.interactable = currentIndex > 0;
+        if (nextButton) nextButton.interactable = currentIndex < panels.Count - 1;
+    }
+
+    private void ChangeActivePanel(int index)
+    {
+        index = Mathf.Clamp(index, 0, panels.Count - 1);
+
+        panels[currentIndex].SetActive(false);
+        panels[index].SetActive(true);
+        currentIndex = index;
+
+        SetButtonsInteractable();
+    }
+
+    /// <summary>
+    /// Use this as the On Click listener for a UI button, to move to the
+    /// previous panel.
+    /// </summary>
+    public void PreviousPanel()
+    {
+        ChangeActivePanel(currentIndex - 1);
+    }
+
+    /// <summary>
+    /// Use this as the On Click listener for a UI button, to move to the
+    /// next panel.
+    /// </summary>
+    public void NextPanel()
+    {
+        ChangeActivePanel(currentIndex + 1);
+    }
+}
---
+++ Assets/UI/UiQuizPanel.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiQuizPanel : MonoBehaviour {
+	/// <summary>Game object used to display title.</summary>
+	public Text QuizQuestion;
+    /// <summary>Prefab used to display one image.</summary>
+	public GameObject ButtonPrefab;
+
+	private List<GameObject> buttonObjects = new List<GameObject>();
+
+	[SerializeField]
+	private UiContent defaultContent = null;
+
+	[ContextMenu("Refresh")]
+	void Start() {
+		if (defaultContent != null) {
+			Display(defaultContent);
+		}
+	}
+
+	/// <summary>Display the given content in the UI</summary>
+	public void Display(UiContent content) {
+		QuizQuestion.text = content.QuizQuestion.ToUpper();
+
+		CacheInstances(ButtonPrefab, content.Images.Count, buttonObjects);
+		for (int i = 0; i < content.QuizAnswers.Count; i++) {
+			var btn = buttonObjects[i].GetComponentInChildren<Button>();
+			var btnText = buttonObjects[i].GetComponentInChildren<Text>();
+
+			btnText.text = content.QuizAnswers[i];
+		}
+	}
+
+	private void CacheInstances<T>(T prefab, int amountWanted, List<T> cache) where T : Object {
+		if (amountWanted > cache.Count) {
+			while (cache.Count < amountWanted) {
+				T instance = (T) Instantiate(prefab, transform);
+				cache.Add(instance);
+			}
+		} else if (amountWanted < cache.Count) {
+			while (cache.Count > amountWanted) {
+				int last = cache.Count - 1;
+				T instance = cache[last];
+				cache.RemoveAt(last);
+				Destroy(instance);
+			}
+		}
+	}
+}
