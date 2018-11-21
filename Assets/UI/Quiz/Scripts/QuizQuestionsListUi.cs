d8a9b96b466956de04adfd99c2fb57e2183ffd0b
Daphne Liu <daphnel15113455@gmail.com>
2018-11-20 22:49:36 -0800

    QuizQuestionListUi converts quizcontent to gameobjects(prefabs)

---
+++ Assets/UI/Quiz/Scripts/QuizQuestionsListUi.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class QuizQuestionsListUi : MonoBehaviour {
+    public GameObject MCQuestionPrefab;
+    public GameObject ShortAnswerQuestionPrefab;  
+
+    private  List<QuizContent> allQuizzes = new List<QuizContent>();
+ 
+    public void Display(List<QuizContent> loQuizcontent) {
+        // Destroy all children 
+        foreach (Transform child in transform) {
+            Object.Destroy(child);
+        }
+
+        // Make new children
+        foreach (var content in loQuizcontent) {
+            GameObject prefab;
+            if (content is MultipleChoice) {
+                prefab = MCQuestionPrefab;
+            } else if (content is ShortAnswer) {
+                prefab = ShortAnswerQuestionPrefab;
+            } else {
+                continue;
+            }
+            Object.Instantiate(prefab, transform); 
+		}
+    }
+}
