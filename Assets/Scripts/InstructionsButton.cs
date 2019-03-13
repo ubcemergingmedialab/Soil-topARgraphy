76b02fed702a305e2a33b322366a5272f655968d
Sophie Berger <berger.m.sophie@gmail.com>
2019-03-13 09:15:19 -0700

    About to onboarding (#28)
    * initial
    
    * initial
    
    * Instructions button added
    
    * Instructions button added
    
    * requested changes made

---
+++ Assets/Scripts/InstructionsButton.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InstructionsButton : MonoBehaviour {
+
+    [SerializeField]
+    private string sceneToOpen;
+    [SerializeField]
+    private bool preloadOnStart;
+
+    private AsyncOperation loadingScene = null;
+
+    void Start()
+    {
+        if (preloadOnStart)
+        {
+            PreloadNewScene();
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        if (loadingScene != null)
+        {
+            loadingScene.allowSceneActivation = true;
+        }
+    }
+
+    public void ExitToNewScene()
+    {
+        StartCoroutine(ExitRoutine());
+    }
+
+    public void PreloadNewScene()
+    {
+        StartCoroutine(PreloadRoutine());
+    }
+
+    private IEnumerator ExitRoutine()
+    {
+        yield return PreloadRoutine();
+        loadingScene.allowSceneActivation = true;
+    }
+
+    private IEnumerator PreloadRoutine()
+    {
+        if (loadingScene == null)
+        {
+            loadingScene = SceneManager.LoadSceneAsync(sceneToOpen, LoadSceneMode.Single);
+            loadingScene.allowSceneActivation = false;
+            yield return loadingScene;
+        }
+    }
+}
